2e3d2e3cb2828a174afab40dd88604e97805095b
Kyle Qi <kyle.et.qi@gmail.com>
2026-06-13 14:32:20 -0400

    commit

---
+++ src/system_wiring.cpp
@@ -0,0 +1,25 @@
+class Compass{
+    public:
+    private:
+}
+
+class Accelerometer{
+    public:
+    private:
+}
+
+class Gyroscope{
+    public:
+    
+    private:
+}
+
+class Magnetometer{
+    public:
+    private:
+}
+
+class GPS{
+    public:
+    private:
+}
